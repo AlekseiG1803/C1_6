9c3768d560597206cd060841372b81844d337986
Aleksei Golovkov <alekseig1803@gmail.com>
2023-05-23 22:12:21 +0300

    Упростил как на лекции

--- Program.cs
+++ Program.cs
@@ -1,30 +1,26 @@
-﻿int number1 = new Random().Next(1, 10);
+﻿int number1 = 1;
+//new Random().Next(1, 10);
 Console.WriteLine(number1);
-int number2 = new Random().Next(1, 10);
+int number2 = 3;
+//new Random().Next(1, 10);
 Console.WriteLine(number2);
-int number3 = new Random().Next(1, 10);
+int number3 = 6;
+//new Random().Next(1, 10);
 Console.WriteLine(number3);
-int number4 = new Random().Next(1, 10);
+int number4 = 9;
+//new Random().Next(1, 10);
 Console.WriteLine(number4);
-int number5 = new Random().Next(1, 10);
+int number5 = 4;
+//new Random().Next(1, 10);
 Console.WriteLine(number5);
+
 int max = number1;
-if(max < number2)
-{
-    max = number2;
-}
-if(max < number3)
-{
-    max = number3;
-}
-if(max < number4)
-{
-    max = number4;
-}
-if(max < number5)
-{
-    max = number5;
-}
+
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+if (number4 > max) max = number4;
+if (number5 > max) max = number5;
+
 Console.Write("Максимум равен: ");
 Console.Write(max);
 
